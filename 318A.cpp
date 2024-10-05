27c777f7cf8b7fe1048f4d56f9d490c8afe613e9
Dishi_Gautam <mail2dishig@gmail.com>
2024-10-05 23:56:31 +0530

    318A solved again

--- 318A.cpp
+++ 318A.cpp
@@ -1,26 +1,16 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-
-void solve(int n,int k){
-    vector<int>v1,v2;
-    for(int i=1;i<=n;i++){
-        if(i%2!=0){
-            v1.push_back(i);
-        }
-        else{
-            v2.push_back(i);
-        }
+int main()
+{
+    long long n, k;
+    cin >> n >> k;
+    if (k <= (n + 1) / 2)
+    {
+        cout << k * 2 - 1 << endl;
     }
-   v1.insert(v1.end(), v2.begin(), v2.end());
-   if(k>0 && k<=v1.size()){
-    cout<<v1[k-1];}
-    else{
-        cout<<"Out of bound";
+    else
+    {
+        cout << (k - (n + 1) / 2) * 2 << endl;
     }
-}
-
-int main(){
-    int n,k;
-    cin>>n>>k;
-    solve(n,k);
+    return 0;
 }
